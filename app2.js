93aea9c1f0689203327fec6c131a04581295e836
21julesjunior <75339052+21julesjunior@users.noreply.github.com>
2020-12-03 11:02:15 +0100

    Update app2.js

--- app2.js
+++ app2.js
@@ -1,3 +1,6 @@
-let age = prompt("What is yout age: ?")
-let spent = age * 375
-alert('Since you are ' + age + ' you have spent ' + spent + ' days on this hearth')
+function Calculate() {
+    let age = document.getElementById('age').value
+    let ha = age * 365
+
+    document.getElementById("test").innerHTML = "You have spent " + ha + " days on this earth !";
+}
